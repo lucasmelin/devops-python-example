5e9912bbee32f7a5ab84cd2367e640e6f073c5f7
Lucas Melin <lucas.melin@gmail.com>
2019-07-21 09:27:59 -0400

    Add redis as a deployment and service.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,10 @@ python manage.py collectstatic --noinput
 echo "Apply database migrations"
 python manage.py migrate
 
+# Start the huey task service
+echo "Starting task queue"
+python manage.py run_huey
+
 # Start server
 echo "Starting server"
 gunicorn -b :8080 lucasmelin_research.wsgi
